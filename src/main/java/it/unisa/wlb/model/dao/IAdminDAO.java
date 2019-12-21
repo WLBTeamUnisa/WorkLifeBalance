cb00bc4aad8f3b172d87820ceca0edfdb888889d
Vinnyeffe <fabianovincenzo27@gmail.com>
2019-12-21 12:26:10 +0100

    Login and Logout Servlet implementation

--- src/main/java/it/unisa/wlb/model/dao/IAdminDAO.java
+++ src/main/java/it/unisa/wlb/model/dao/IAdminDAO.java
@@ -3,5 +3,7 @@ package it.unisa.wlb.model.dao;
 import it.unisa.wlb.model.bean.Admin;
 
 public interface IAdminDAO extends IGenericDAO<Admin> {
-
+	
+	public Admin retrieveByEmailPassword(String email, String password);
+	
 }
---
+++ src/test/java/it/unisa/wlb/controller/LogoutServletTest.java
@@ -0,0 +1,7 @@
+package it.unisa.wlb.controller;
+
+import junit.framework.TestCase;
+
+public class LogoutServletTest extends TestCase {
+
+}
