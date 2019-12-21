f1e0e07d9ed20bae3a42fade191283cf165ea8ed
mickID <52219436+mickID@users.noreply.github.com>
2019-12-21 13:07:38 +0100

    Modified AdminJpa

--- src/main/java/it/unisa/wlb/model/jpa/AdminJpa.java
+++ src/main/java/it/unisa/wlb/model/jpa/AdminJpa.java
@@ -1,48 +1,49 @@
 package it.unisa.wlb.model.jpa;
 
 import java.util.List;
-import javax.inject.Inject;
+
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import it.unisa.wlb.model.bean.Admin;
 import it.unisa.wlb.model.dao.IAdminDAO;
 
+@Stateless
 public class AdminJpa implements IAdminDAO {
   private static final EntityManagerFactory factor =
       Persistence.createEntityManagerFactory("WorkLifeBalance");
-  private EntityManager entitymanager;
+  private EntityManager entityManager;
 
   public Admin create(Admin entity) {
-    entitymanager = factor.createEntityManager();
-    entitymanager.getTransaction().begin();
-    entitymanager.persist(entity);
-    entitymanager.getTransaction().commit();
+    entityManager = factor.createEntityManager();
+    entityManager.getTransaction().begin();
+    entityManager.persist(entity);
+    entityManager.getTransaction().commit();
     return entity;
   }
 
   @Override
   public void remove(Admin entityClass) {
-    entitymanager.getTransaction().begin();
-    entitymanager.remove(entityClass);
-    entitymanager.getTransaction().commit();
+    entityManager.getTransaction().begin();
+    entityManager.remove(entityClass);
+    entityManager.getTransaction().commit();
   }
 
   @Override
   public Admin update(Admin entityClass) {
-    entitymanager.getTransaction().begin();
-    entitymanager.merge(entityClass);
-    entitymanager.getTransaction().commit();
+    entityManager.getTransaction().begin();
+    entityManager.merge(entityClass);
+    entityManager.getTransaction().commit();
     return entityClass;
   }
 
   @Override
   public List<Admin> retrieveAll() {
-    entitymanager.getTransaction().begin();
-    Query q = entitymanager.createQuery("SELECT admin FROM Admin admin");
-    entitymanager.getTransaction().commit();
+    entityManager.getTransaction().begin();
+    Query q = entityManager.createQuery("SELECT admin FROM Admin admin");
+    entityManager.getTransaction().commit();
 
     return (List<Admin>) q.getResultList();
   }
