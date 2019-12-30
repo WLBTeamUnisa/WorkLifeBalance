96d227918137eda80d4dfeef852f07c6ae0f4e60
Sabato Nocera <nocera.sabato@protonmail.com>
2019-12-30 18:11:59 +0100

    PlanimetryInsertionPageServlet.java and PlanimetryInsertion.jsp have been updated so that if the planimetry is already inserted, it will be displayed the current number of workstation of each room of each floor

--- src/main/java/it/unisa/wlb/controller/PlanimetryInsertionPageServlet.java
+++ src/main/java/it/unisa/wlb/controller/PlanimetryInsertionPageServlet.java
@@ -1,17 +1,39 @@
 package it.unisa.wlb.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+import it.unisa.wlb.model.bean.Room;
+import it.unisa.wlb.model.dao.IRoomDao;
+
 /**
- * Servlet implementation class PlanimetryInsertionPageServlet
+ * Servlet implementation class PlanimetryInsertionPageServlet. It is used to access to PlanimetryInsertion.jsp.
+ * 
+ * @author Sabato
+ *
  */
 @WebServlet("/PlanimetryInsertionPage")
 public class PlanimetryInsertionPageServlet extends HttpServlet {
+	
+	private final static String FLOOR = "floor";
+	private final static String ROOM = "room";
+	private final static String WORKSTATIONS = "workstation";
+	private final static String PLANIMETRY = "insertedPlanimetry";
+	
+	@EJB
+	private IRoomDao roomDao;
+	
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -19,17 +41,35 @@ public class PlanimetryInsertionPageServlet extends HttpServlet {
      */
     public PlanimetryInsertionPageServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		if(!(request.getSession().getAttribute("user")==null) || !(request.getSession().getAttribute("userRole").equals("Admin"))) {
 			//exception
+		}
+		
+		try {
+			List<Room> rooms = roomDao.retrieveAll();
+			if(rooms!=null && rooms.size()>0) {
+				JSONArray jsonArray = new JSONArray();
+				for(Room room :  rooms) {
+					JSONObject jsonObject = new JSONObject();
+					jsonObject.put(FLOOR, room.getId().getNumFloor());
+					jsonObject.put(ROOM, room.getId().getNumRoom());
+					jsonObject.put(WORKSTATIONS, room.getWorkstations().size());
+					
+					System.err.println(jsonObject.toString());
+					
+					jsonArray.put(jsonObject);				
+				}
+
+				request.setAttribute(PLANIMETRY, jsonArray.toString());				
+			}			
+		} catch (Exception e) {
+			response.getWriter().append("Planimetria assente nel database");
 		}
 		
 		request.getRequestDispatcher("WEB-INF/PlanimetryInsertion.jsp").forward(request, response);
@@ -39,7 +79,6 @@ public class PlanimetryInsertionPageServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
