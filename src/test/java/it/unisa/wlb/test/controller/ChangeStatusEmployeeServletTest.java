d4f410fe64415b087848351d269409aee3a81569
ETesauro <emmanuel.tesauro@gmail.com>
2020-01-04 21:23:41 +0100

    Update ChangeStatusEmployee Test

--- src/test/java/it/unisa/wlb/test/controller/ChangeStatusEmployeeServletTest.java
+++ src/test/java/it/unisa/wlb/test/controller/ChangeStatusEmployeeServletTest.java
@@ -1,4 +1,4 @@
-package it.unisa.wlb.test;
+package it.unisa.wlb.test.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
