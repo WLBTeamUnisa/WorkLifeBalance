28e4d024b06d624755238ea0917f70f49eea841d
Luigi Cerrone <cerrone.luigi@yahoo.it>
2020-01-06 15:35:36 +0100

    Bugs fixed

--- src/main/java/it/unisa/wlb/controller/ShowSmartWorkingPrenotationServlet.java
+++ src/main/java/it/unisa/wlb/controller/ShowSmartWorkingPrenotationServlet.java
@@ -1,15 +1,20 @@
 package it.unisa.wlb.controller;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
+import javax.persistence.NoResultException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,9 +32,10 @@ import it.unisa.wlb.model.dao.ISmartWorkingPrenotationDAO;
 public class ShowSmartWorkingPrenotationServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	private static final Calendar CALENDAR = Calendar.getInstance();
+	private Calendar CALENDAR = Calendar.getInstance();
 	private SmartWorkingPrenotation smartWorkingPrenotation;
 	private Employee employee;
+	
 	@EJB
 	private ISmartWorkingPrenotationDAO smartWorkingDao;
 
@@ -38,45 +44,35 @@ public class ShowSmartWorkingPrenotationServlet extends HttpServlet {
 			request.getRequestDispatcher("WEB-INF/Index.jsp").forward(request, response);
 		}
 		else {
+			CALENDAR = Calendar.getInstance();
+			SmartWorkingPrenotation smartWorkingPrenotation;
+			Employee employee;
 			employee = (Employee) request.getSession().getAttribute("user");
 			TimeZone tz = CALENDAR.getTimeZone();
 			ZoneId zid = tz == null ? ZoneId.systemDefault() : tz.toZoneId();
 			LocalDate today = LocalDateTime.ofInstant(CALENDAR.toInstant(), zid).toLocalDate();
-			CALENDAR.setTime(Date.from(today.atStartOfDay().atZone(zid).toInstant()));
+			
+			LocalDate nextMonday = today.with(DayOfWeek.MONDAY);
+			System.out.println(nextMonday);
+			LocalDate newDate;
+			newDate= nextMonday.plusDays(7);
+			System.out.println(newDate);
+			CALENDAR.setTime(Date.from(newDate.atStartOfDay().atZone(zid).toInstant()));
 			int currentCalendarWeek = CALENDAR.get(Calendar.WEEK_OF_YEAR);
+			System.out.println(currentCalendarWeek);
 			int currentYear = CALENDAR.get(Calendar.YEAR);
-			int lastWeekOfYear = CALENDAR.getActualMaximum(Calendar.WEEK_OF_YEAR);
-			
-			if(lastWeekOfYear==currentCalendarWeek) {
-				
+			System.out.println(currentYear);
 				try {
-					smartWorkingPrenotation = smartWorkingDao.retrieveByWeeklyPlanning(1, currentYear+1, employee.getEmail());
+					smartWorkingPrenotation = smartWorkingDao.retrieveByWeeklyPlanning(currentCalendarWeek, currentYear, employee.getEmail());
+					request.setAttribute("booking", "no");
+					request.getRequestDispatcher("WEB-INF/SmartWorkingPrenotation.jsp").forward(request, response);
+					
 				} catch(EJBException e) {
 					request.setAttribute("booking", "yes");
 					request.getRequestDispatcher("WEB-INF/SmartWorkingPrenotation.jsp").forward(request, response);
 				} catch(Exception e) {
-					//implementare comportamento
-					e.printStackTrace();
+					
 				}
-				
-			} else {
-				
-				try {
-					smartWorkingPrenotation = smartWorkingDao.retrieveByWeeklyPlanning(currentCalendarWeek+1, currentYear, employee.getEmail());
-				} catch(EJBException e) {
-					request.setAttribute("booking", "yes");
-					request.getRequestDispatcher("WEB-INF/SmartWorkingPrenotation.jsp").forward(request, response);
-				} catch(Exception e) {
-					//implementare comportamento
-					e.printStackTrace();
-				}
-				
-			}
-			
-			if(smartWorkingPrenotation != null) {
-				request.setAttribute("booking", "no");
-				request.getRequestDispatcher("WEB-INF/SmartWorkingPrenotation.jsp").forward(request, response);
-			}
 		}
 	}
 
