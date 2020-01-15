e358c96bd52e2b85a2e12dceb88c6b5b45ef92c0
Luigi Cerrone <cerrone.luigi@yahoo.it>
2020-01-15 12:36:02 +0100

    Logger Fixing

--- src/main/java/it/unisa/wlb/utils/LoggerSingleton.java
+++ src/main/java/it/unisa/wlb/utils/LoggerSingleton.java
@@ -1,9 +1,9 @@
 package it.unisa.wlb.utils;
-
 import java.util.Date;
-
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 /**
  * The aim of this logger is printing a message when a method is called
@@ -12,7 +12,6 @@ import javax.interceptor.InvocationContext;
  *
  */
 public class LoggerSingleton {
-
 	/**
 	 * Method for logging
 	 * 
@@ -24,9 +23,10 @@ public class LoggerSingleton {
 	@AroundInvoke
 	public Object logMethod(InvocationContext invocationContext) throws Exception
 	{
+		Logger.getLogger("org.hibernate").setLevel(Level.OFF);
 		Date entryDate=new Date();
 		String methodName=invocationContext.getMethod().getName();
-		String classOfMethod=invocationContext.getTarget().getClass().getSimpleName();
+		String classOfMethod=invocationContext.getTarget().getClass().getName();
 		System.out.println("Calling the method "+methodName +" of "+classOfMethod+" class to the instant "+entryDate.toLocaleString());
 		try {
 			return invocationContext.proceed();
